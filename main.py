5c0ba2e5e94f524a01896586c4bf376058eaf399
xAesir <sajovara@proton.me>
2023-03-18 20:14:26 +0100

    /todo-list Enpoint funktioniert

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import *
+import uuid
 
 allLists = []
 
@@ -6,33 +7,44 @@ allLists = []
 app = Flask(__name__)
 
 # Route fürs Zurückgeben von Entries in einer Liste
-@app.route('/todo-list/<list_id>/entries ', methods = ['GET'])
+@app.route('/todo-list/<list_id>/entries', methods = ['GET'])
 def return_entries_of_list(list_id):
     return 
 
 # Route fürs Löschen von Todo-Listen
-@app.route('/todo-list/<list_id> ', methods = ['DELETE'])
+@app.route('/todo-list/<list_id>', methods = ['DELETE'])
 def delete_list(list_id):
-    return 
+    return
 
 # Route fürs Erstellen von Todo-Listen
 @app.route('/todo-list', methods = ['PUT'])
 def create_list():
-    return 
+    list_name = request.form.get('name')#
+    #if not name:
+        
+    list_uuid = str(uuid.uuid4())
+    dictionary = {
+        'id' : list_uuid,
+        'name' : list_name
+    }
+
+    allLists.append(dictionary)
+
+    return jsonify(dictionary)
 
 # Route um einen Eintrag zu einer Todo-Liste hinzuzufügen
 @app.route('/entries', methods = ['POST'])
-def index():
+def add_entry_to_list():
     return 
 
 # Route um einen bestehenden Eintrag zu aktualisieren
 @app.route('/entries/<list_id>/<entries_id>', methods = ['POST'])
-def index(list_id, entries_id):
+def update_entry_in_list(list_id, entries_id):
     return 
 
 # Route um einen bestehenden Eintrag einer Todo-Liste zu löschen
 @app.route('/entries/<list_id>/<entries_id>', methods = ['DELETE'])
-def index(list_id, entries_id):
+def delete_enty_from_list(list_id, entries_id):
     return 
 
 if __name__ == '__main__':
