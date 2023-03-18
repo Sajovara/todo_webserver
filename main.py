c30f5c525c02fc69bb4dd3235fc0c11e65ded222
xAesir <sajovara@proton.me>
2023-03-18 21:14:25 +0100

    Die Enpunkte sollten alle erstmal klappen bis wir im Unterricht andere Anforderungen besprechen

--- main.py
+++ main.py
@@ -6,15 +6,7 @@ allEntries = []
 
 # initialisiere Flask-Server
 app = Flask(__name__)
-
-def findList(list_uuid):
-    for dictionary in allLists:
-        if(dictionary['uuid'] == list_uuid):
-            return dictionary
         
-def findCorrespondingEntries(list_uuid):
-    
-
 # Route fürs Zurückgeben von Entries in einer Liste
 @app.route('/todo-list/<list_id>/entries', methods = ['GET'])
 def return_entries_of_list(list_id):
@@ -22,7 +14,7 @@ def return_entries_of_list(list_id):
     foundEntries = []
 
     for dictionary in allEntries:
-        if(dictionary['list_reference'] == list_id):
+        if dictionary['list_reference'] == list_id:
             foundEntries.append(dictionary)
             
     return foundEntries
@@ -30,19 +22,20 @@ def return_entries_of_list(list_id):
 # Route fürs Löschen von Todo-Listen
 @app.route('/todo-list/<list_id>', methods = ['DELETE'])
 def delete_list(list_id):
-    return
+
+    for dictionary in allLists:
+        if(dictionary['uuid'] == list_id):
+            list.pop(dictionary)
+
+    return jsonify({'deleted' : 'true'})
 
 # Route fürs Erstellen von Todo-Listen
 @app.route('/todo-list', methods = ['PUT'])
 def create_list():
-    list_name = request.form.get('name')#
-    #if not name:
-        
-    list_uuid = str(uuid.uuid4())
-    dictionary = {
-        'id' : list_uuid,
-        'name' : list_name
 
+    dictionary = {
+        'id' : str(uuid.uuid4()),
+        'name' : request.form.get('name')
     }
 
     allLists.append(dictionary)
@@ -52,17 +45,43 @@ def create_list():
 # Route um einen Eintrag zu einer Todo-Liste hinzuzufügen
 @app.route('/entries', methods = ['POST'])
 def add_entry_to_list():
-    return 
 
-# Route um einen bestehenden Eintrag zu aktualisieren
-@app.route('/entries/<list_id>/<entries_id>', methods = ['POST'])
-def update_entry_in_list(list_id, entries_id):
-    return 
+    if request.form.get('desc'):
+        description = request.form.get('desc')
+    else:
+        description = ""
+    
+    dictionary = {
+        'list_reference' : request.form.get('list_id'),
+        'id' : str(uuid.uuid4()),
+        'name' : request.form.get('name'),
+        'desc' : description
+    }
 
-# Route um einen bestehenden Eintrag einer Todo-Liste zu löschen
-@app.route('/entries/<list_id>/<entries_id>', methods = ['DELETE'])
-def delete_enty_from_list(list_id, entries_id):
-    return 
+    allEntries.append(dictionary)
+
+    return jsonify(dictionary)
+
+# Route um einen bestehenden Eintrag zu aktualisieren oder zu löschen
+@app.route('/entries/<list_id>/<entries_id>', methods = ['POST', 'DELETE'])
+def update_or_delete_entry_in_list(list_id, entries_id):
+
+    counter = 0
+
+    if request.method == 'POST':
+        for dictionary in allEntries:
+            if dictionary['list_reference'] == list_id and dictionary['id'] == entries_id:
+                for element in request.form.keys:
+                    dictionary[element] = request.form.get(element) 
+
+        return jsonify({'updated' : counter})
+    else:
+        for dictionary in allEntries:
+            if dictionary['list_reference'] == list_id and dictionary['id'] == entries_id:
+                counter += 1
+                allEntries.pop(dictionary)
+        
+        return jsonify({'deleted' : counter})
 
 if __name__ == '__main__':
  app.run(host='0.0.0.0', port=5000, debug=True)
