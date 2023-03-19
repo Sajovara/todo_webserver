a5e626b27a43c2a05b6c69ba4a5ac7499987b6f3
xAesir <sajovara@proton.me>
2023-03-19 14:10:29 +0100

    Robusten Client gebaut, es funktioniert alles tiptop

--- client.py
+++ client.py
@@ -8,7 +8,9 @@ def clearScreen():
 
 def startUp():
     clearScreen()
+
     print("Was möchtest du tun?")
+    print()
     print("1. Eine neue Liste erstellen")
     print("2. Eine Liste löschen")
     print("3. Einen Eintrag erstellen")
@@ -18,6 +20,7 @@ def startUp():
     print("7. Einen Eintrag löschen")
     print("8. Alle Einträge einer Liste anzeigen")
     print("9. Gar nichts")
+    print()
 
     choice = 0
 
@@ -32,131 +35,127 @@ def startUp():
 
     return int(choice)
 
-while stillInMenu:
-    
-    match startUp():
+def listSelector(prompt):
+    clearScreen()
+    listLister = r.get("http://127.0.0.1:5000/todo-list/search")
 
-        case 1:
-            clearScreen()  
-            name = input("Wie soll die neue To-Do Liste heißen? ")
-            while len(name) <= 0:
-                name = input("Wie soll die neue To-Do Liste heißen? ")
-            result = r.put("http://127.0.0.1:5000/todo-list", data={'name' : name})
-            clearScreen()
+    i = 0
+
+    if len(listLister.json()) == 0:
+        print("Es existiert noch keine Liste")
+        print()
+        input("Bitte Enter drücken...")
+        return False
+    else:
+        print("Folgende Listen existieren:")
+        print()
+        while i < len(listLister.json()):
+                print("Liste " + str(i+1) + ": " + listLister.json()[i]['name'])
+                i += 1
+        print()
+        
+        choice = 0
+
+        while True:
+            if (choice == "" or not str(choice).isdigit()) or (int(choice) <= 0 or int(choice) >= 4) or (int(choice) > len(listLister.json())):
+                
+                match prompt:
+                    
+                    case 'delete': choice = input("Welche Liste möchtest du löschen? (1-"+str(i)+"): ")
+
+                    case 'create_entry': choice = input("Zu welcher Liste möchtest du einen Eintrag hinzufügen? (1-"+str(i)+"): ")
+
+                    case 'update_entry': choice = input("In welcher Liste möchtest du einen Eintrag aktualisieren? (1-"+str(i)+"): ")
+
+                    case 'update_entry_list': choice = input("Welcher Liste möchtest du den Eintrag zuordnen? (1-"+str(i)+"): ")
+
+                    case 'delete_entry': choice = input("Aus welcher Liste möchtest du einen Eintrag löschen? (1-"+str(i)+"): ")
+
+                    case 'display_entries': choice = input("Aus welcher Liste möchtest du die Einträge einsehen? (1-"+str(i)+"): ")
+                
+            else:
+                clearScreen()
+                return str(listLister.json()[int(choice)-1]['id'])
+            
+
+def entrySelector(prompt, list_id):
+    if list_id is not False:
+        clearScreen()
+        entryLister = r.get("http://127.0.0.1:5000/todo-list/"+str(list_id)+"/entries")
+
+        i = 0
+
+        if len(entryLister.json()) == 0:
+            print("Es existieren noch keine Einträge in dieser Liste")
+            print()
+            input("Bitte Enter drücken...")
+            return False
+        else:
+            print("Folgende Einträge existieren:")
+            print()
+            while i < len(entryLister.json()):
+                    print("Eintrag " + str(i+1) + ": " + entryLister.json()[i]['name'])
+                    i += 1
+            print()
+            
+            choice = 0
+
+            while True:
+                if (choice == "" or not str(choice).isdigit()) or (int(choice) <= 0 or int(choice) >= 4) or (int(choice) > len(entryLister.json())):
+                    
+                    match prompt:
+                        
+                        case 'delete_entry': choice = input("Welchen Eintrag möchtest du löschen? (1-"+str(i)+"): ")
+
+                        case 'update_entry': choice = input("Welchen Eintrag möchtest du aktualisieren? (1-"+str(i)+"): ")
+                    
+                else:
+                    clearScreen()
+                    return str(entryLister.json()[int(choice)-1]['id'])
+    else:
+        return False
+            
+def printResult(result, prompt):
+    clearScreen()
+    
+    match prompt:
+        
+        case 'create_list': 
             print("Folgende neue Liste wurde angelegt:")
+            print()
             print("ID: " + result.json()['id'])
             print("Name: " + result.json()['name'])
-            input("Bitte Enter drücken...")
         
-        case 2:
-            clearScreen()  
-            list_id = input("Welche Liste möchtest du löschen? (ID): ")
-            result = r.delete("http://127.0.0.1:5000/todo-list/" + str(list_id))
-            clearScreen()
+        case 'delete_list':
             if result.json()['deleted']:
                 print("Die Liste wurde erfolgreich gelöscht")
             else:
-                print("Löschen nicht erfolgreich (gibt es die Liste?)")
-            input("Bitte Enter drücken...")
+                print("Löschen nicht erfolgreich")
 
-        case 3:
-            clearScreen()
-            list_reference = input("Zu welcher Liste soll der Eintrag gehören? (ID): ")
-            name = input("Wie soll der neue Eintrag heißen? ")
-            description = input("Wie soll die Beschreibung lauten? (darf leer sein): ")
-            result = r.post("http://127.0.0.1:5000/entries", data={'name' : name, 'list_reference' : list_reference, 'desc' : description})
-            clearScreen()
-            if result.json()['list_reference'] == "":
-                print("Die Liste existiert nicht")
-            else: 
-                print("Der folgende Eintrag wurde hinzugefügt:")
-                print("ID: " + result.json()['id'])
-                print("Listenreferenz: " + result.json()['list_reference'])
-                print("Name: " + result.json()['name'])
-                print("Beschreibung: " + result.json()['desc'])
-            input("Bitte Enter drücken...")
+        case 'create_entry':
+            print("Der folgende Eintrag wurde hinzugefügt:")
+            print()
+            print("ID: " + result.json()['id'])
+            print("Listenreferenz: " + result.json()['list_reference'])
+            print("Name: " + result.json()['name'])
+            print("Beschreibung: " + result.json()['desc'])
 
-        case 4:
-            clearScreen()
-            list_id = input("In welcher Liste möchtest du einen Eintrag aktualisieren? (ID): ")
-            entry_id = input("Um welchen Eintrag handelt es sich? (ID): ")
+        case 'update_entry':
+            print("Der Eintrag sieht nun wie folgt aus: ")
+            print()
+            print("ID: " + result.json()['id'])
+            print("Listenreferenz: " + result.json()['list_reference'])
+            print("Name: " + result.json()['name'])
+            print("Beschreibung: " + result.json()['desc'])
 
-            name = input("Bitte gib den neuen Namen des Eintrags ein: ")
-            
-            clearScreen()
-            result = r.post("http://127.0.0.1:5000/entries/" + str(list_id) + "/" + entry_id, data={'name' : name, 'type' : 'name'})
-
-            if not result.json():
-                print("Die Liste oder der Eintrag existiert nicht")
-            else:
-                print("Der Eintrag sieht nun wie folgt aus: ")
-                print("ID: " + result.json()['id'])
-                print("Listenreferenz: " + result.json()['list_reference'])
-                print("Name: " + result.json()['name'])
-                print("Beschreibung: " + result.json()['desc'])
-            input("Bitte Enter drücken...")
-
-        case 5:
-            clearScreen()
-            list_id = input("In welcher Liste möchtest du einen Eintrag aktualisieren? (ID): ")
-            entry_id = input("Um welchen Eintrag handelt es sich? (ID): ")
-
-            description = input("Bitte gib die neue Beschreibung des Eintrags ein: ")
-            
-            clearScreen()
-            result = r.post("http://127.0.0.1:5000/entries/" + str(list_id) + "/" + str(entry_id), data={'desc' : description, 'type' : 'desc'})
-
-            if not result.json():
-                print("Die Liste oder der Eintrag existiert nicht")
-            else:
-                print("Der Eintrag sieht nun wie folgt aus: ")
-                print("ID: " + result.json()['id'])
-                print("Listenreferenz: " + result.json()['list_reference'])
-                print("Name: " + result.json()['name'])
-                print("Beschreibung: " + result.json()['desc'])
-            input("Bitte Enter drücken...")
-
-        case 6:
-            clearScreen()
-            list_id = input("In welcher Liste möchtest du einen Eintrag aktualisieren? (ID): ")
-            entry_id = input("Um welchen Eintrag handelt es sich? (ID): ")
-
-            list_reference = input("Bitte gib die neue Listenzuordnung des Eintrags ein (ID): ")
-            
-            clearScreen()
-            result = r.post("http://127.0.0.1:5000/entries/" + str(list_id) + "/" + str(entry_id), data={'list_reference' : list_reference, 'type' : 'list_reference'})
-
-            if not result.json():
-                print("Die Liste oder der Eintrag existiert nicht")
-            else:
-                print("Der Eintrag sieht nun wie folgt aus: ")
-                print("ID: " + result.json()['id'])
-                print("Listenreferenz: " + result.json()['list_reference'])
-                print("Name: " + result.json()['name'])
-                print("Beschreibung: " + result.json()['desc'])
-            input("Bitte Enter drücken...")
-
-        case 7:
-            clearScreen()
-            list_id = input("In welcher Liste möchtest du einen Eintrag löschen? (ID): ")
-            entry_id = input("Um welchen Eintrag handelt es sich? (ID): ")
-            
-            clearScreen()
-            result = r.delete("http://127.0.0.1:5000/entries/" + str(list_id) + "/" + str(entry_id))
-            if result:
+        case 'delete_entry':
+            if result.json()['deleted']:
                 print("Der Eintrag wurde gelöscht")
             else: 
                 print("Der Eintrag konnte nicht gelöscht werden")
-            input("Bitte Enter drücken...")
 
-        case 8:
-            clearScreen()
-            list_id = input("Von welcher Liste möchstest du alle Einträge anzeigen? (ID): ")
-
-            clearScreen()
-            result = r.get("http://127.0.0.1:5000/todo-list/" + str(list_id) + "/entries")
-
-            if result.json() is not False:
+        case 'display_entries':
+            if len(result.json()) >= 1:
                 i = 0
                 while i < len(result.json()):
                     print("Eintrag " + str(i+1) + ":")
@@ -168,6 +167,76 @@ while stillInMenu:
                     print()
                     i += 1
             else:
-                print("Die Liste existiert nicht")
+                print("Die Liste hat noch keine Einträge")
+    
+    print()
+    input("Bitte Enter drücken...")
 
-            input("Bitte Enter drücken...")
+while stillInMenu:
+    
+    match startUp():
+
+        case 1:
+            clearScreen()  
+            name = input("Wie soll die neue To-Do Liste heißen? ")
+            while len(name) <= 0:
+                name = input("Wie soll die neue To-Do Liste heißen? ")
+            result = r.put("http://127.0.0.1:5000/todo-list", data={'name' : name})
+            printResult(result, 'create_list')
+        
+        case 2:
+            list_selected =  listSelector('delete')
+
+            if list_selected is not False:
+                result = r.delete("http://127.0.0.1:5000/todo-list/" + list_selected)
+                printResult(result, 'delete_list')
+
+        case 3:
+            list_selected = listSelector('create_entry')
+            if list_selected is not False:
+                name = input("Wie soll der neue Eintrag heißen? ")
+                description = input("Wie soll die Beschreibung lauten? (darf leer sein): ")
+                result = r.post("http://127.0.0.1:5000/entries", data={'name' : name, 'list_reference' : list_selected, 'desc' : description})
+                printResult(result, 'create_entry')
+
+        case 4:
+            list_selected = listSelector('update_entry')
+            entry_selected = entrySelector('update_entry', list_selected)
+
+            if entry_selected is not False:
+                name = input("Bitte gib den neuen Namen des Eintrags ein: ")
+                result = r.post("http://127.0.0.1:5000/entries/" + list_selected + "/" + entry_selected, data={'name' : name, 'type' : 'name'})
+                printResult(result, 'update_entry')
+
+        case 5:
+            list_selected = listSelector('update_entry')
+            entry_selected = entrySelector('update_entry', list_selected)
+
+            if entry_selected is not False:
+                description = input("Bitte gib die neue Beschreibung des Eintrags ein: ")
+                result = r.post("http://127.0.0.1:5000/entries/" + list_selected + "/" + entry_selected, data={'desc' : description, 'type' : 'desc'})
+                printResult(result, 'update_entry')
+
+        case 6:
+            list_selected = listSelector('update_entry')
+            entry_selected = entrySelector('update_entry', list_selected)
+
+            if entry_selected is not False:
+                list_reference = listSelector('update_entry_list')
+                result = r.post("http://127.0.0.1:5000/entries/" + list_selected + "/" + entry_selected, data={'list_reference' : list_reference, 'type' : 'list_reference'})
+                printResult(result, 'update_entry')
+
+        case 7:
+            list_selected = listSelector('delete_entry')
+            entry_selected = entrySelector('delete_entry', list_selected)
+            
+            if list_selected is not False and entry_selected is not False:
+                result = r.delete("http://127.0.0.1:5000/entries/" + list_selected + "/" + entry_selected)
+                printResult(result, 'delete_entry')
+
+        case 8:
+            list_selected = listSelector('display_entries')
+
+            if list_selected is not False:
+                result = r.get("http://127.0.0.1:5000/todo-list/" + list_selected + "/entries")
+                printResult(result, 'display_entries')
--- main.py
+++ main.py
@@ -11,21 +11,12 @@ app = Flask(__name__)
 @app.route('/todo-list/<list_id>/entries', methods = ['GET'])
 def return_entries_of_list(list_id):
 
-    listExists = False
-
-    for dictionary in allLists:
-        if dictionary['id'] == list_id:
-            listExists = True
-
     foundEntries = []
-    if listExists:
-        for dictionary in allEntries:
-            if dictionary['list_reference'] == list_id:
-                foundEntries.append(dictionary)
+    for dictionary in allEntries:
+        if dictionary['list_reference'] == list_id:
+            foundEntries.append(dictionary)
                 
-        return jsonify(foundEntries)
-    else:
-        return jsonify(False)
+    return jsonify(foundEntries)
 
 # Route fürs Löschen von Todo-Listen
 @app.route('/todo-list/<list_id>', methods = ['DELETE'])
@@ -56,20 +47,9 @@ def create_list():
 # Route um einen Eintrag zu einer Todo-Liste hinzuzufügen
 @app.route('/entries', methods = ['POST'])
 def add_entry_to_list():
-
-    list_reference = request.form.get('list_reference')
-
-    listExists = False
-
-    for dictionary in allLists:
-        if(dictionary['id'] == list_reference):
-            listExists = True
-
-    if not listExists:
-        list_reference = ""
     
     dictionary = {
-        'list_reference' : list_reference,
+        'list_reference' : request.form.get('list_reference'),
         'id' : str(uuid.uuid4()),
         'name' : request.form.get('name'),
         'desc' : request.form.get('desc')
@@ -93,13 +73,20 @@ def update_or_delete_entry_in_list(list_id, entries_id):
                 elif request.form.get('type') == 'list_reference':
                     dictionary['list_reference'] = request.form.get('list_reference')
             return jsonify(dictionary)
-        return jsonify(False)
     else:
+        deleted = False
         for dictionary in allEntries:
             if dictionary['list_reference'] == list_id and dictionary['id'] == entries_id:
                 allEntries.remove(dictionary)
-                return jsonify(True)
-        return jsonify(False)
+                deleted = True
+        return jsonify({'deleted' : deleted})
         
+
+# Route fürs zurückgeben aller Listen
+@app.route('/todo-list/search/', methods = ['GET'])
+def return_list_of_lists():
+
+    return jsonify(allLists)
+
 if __name__ == '__main__':
  app.run(host='0.0.0.0', port=5000, debug=True)
